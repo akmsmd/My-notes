3dda10848dbb10e45174a6f950ab930407c03dbe
Alao Kolapo <alaokolapo7@gmail.com>
2022-02-25 10:26:48 +0100

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:my_notesss/login_view.dart';
 import 'package:my_notesss/register_view.dart';
 import 'firebase_options.dart';
 
@@ -14,7 +13,7 @@ void main() {
     theme: ThemeData(
       primarySwatch: Colors.blue,
     ),
-    home: const LoginView(),
+    home: const HomePage(),
   ));
 }
 class HomePage extends StatelessWidget {
