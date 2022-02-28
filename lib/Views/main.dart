04392d5a1e1bdba52cef4e548b900e1021e4700f
Alao Kolapo <alaokolapo7@gmail.com>
2022-02-28 11:08:17 +0100

    Initial Commit 2

---
+++ lib/Views/main.dart
@@ -0,0 +1,56 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:my_notesss/Views/login_view.dart';
+import 'package:my_notesss/Views/register_view.dart';
+import 'package:my_notesss/Views/verify_email_view.dart';
+import '../firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(      MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+    ),
+    home: const HomePage(),
+
+    routes: {
+      '/login/': (context) => const LoginView(),
+      '/register/': (context) => const RegisterView(),
+    },
+  ));
+}
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+          final user = FirebaseAuth.instance.currentUser;
+          if(user != null){
+            if(user.emailVerified){
+              print('Email Is verified');
+            }else{
+              return const VerifyEmailView();
+            }
+          }else{
+            return const LoginView();
+          }return const Text('Done');
+          default:
+            return const CircularProgressIndicator();
+       }
+      },
+
+    );
+  }
+}
+
+
